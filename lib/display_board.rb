c1351588caf62762f891dfab687500ec1ef2296a
Rohit Ukirde <ukirderohit@gmail.com>
2018-07-04 03:09:17 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board(x)
+def display_board(x="   ")
   puts " #{x[0]} "+"|"+" #{x[1]} "+"|"+" #{x[2]} "
   puts "-----------"
   puts " #{x[4]} "+"|"+" #{x[5]} "+"|"+" #{x[6]} "
